049561525608ed6cdcc136ecc41558d258647a69
Lydiahkh <lkhotieno@gmail.com>
2025-05-01 22:11:01 +0300

    added basic form heading and added typwriter plug in

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("Hello World");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#poem", {
+    strings: "A huge ego breaks off love’s embrace",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
